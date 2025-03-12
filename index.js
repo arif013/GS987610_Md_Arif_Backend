9c5f54228d419a051f9648bcfed23b81866dcba1
arif013 <mdarif11099@gmail.com>
2025-03-13 02:21:48 +0530

    generic cors

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 // app.use(cors({ origin: 'https://gs-987610-md-arif.vercel.app/' }));
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://gs-987610-md-arif.vercel.app', 'https://gs-987610-md-arif.vercel.app/login'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: ['http://localhost:5173', 'https://gs-987610-md-arif.vercel.app', 'https://gs-987610-md-arif.vercel.app/login'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+// }));
+app.use(cors())
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
