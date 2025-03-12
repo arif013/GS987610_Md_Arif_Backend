ab7047f9a9cb3cfa4abe1778f425b17bc9ae99f2
arif013 <mdarif11099@gmail.com>
2025-03-13 02:09:00 +0530

    cors value changed

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ import cors from 'cors'
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://gs-987610-md-arif.vercel.app/' }));
+// app.use(cors({ origin: 'https://gs-987610-md-arif.vercel.app/' }));
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://gs-987610-md-arif.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
